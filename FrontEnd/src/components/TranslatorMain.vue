3cefcec2b682f97cfe42eb3dcae995d1da013a47
Rotem Albukrek <Rotem.Albukrek@gmail.com>
2025-06-22 23:23:50 +0300

    Fix(TransltorMain.vue,entrpoint): lf vs clrf and vuex getter

--- FrontEnd/src/components/TranslatorMain.vue
+++ FrontEnd/src/components/TranslatorMain.vue
@@ -58,7 +58,7 @@ export default {
       return this.getDialects
     },
     activeBackendUrl () {
-      return this.getActiveBackendUrl
+      return this.getActiveBackend
     }
   },
   methods: {
